bda61347d5ccaa42bb2fce203e27da19bf47827a
Deepak Rai <deepakmegtech@gmail.com>
2024-07-23 05:41:53 +0530

    Multiple reducer created and used in combineReducer

---
+++ redux_complex_state_using/src/store/cartReducer.js
@@ -0,0 +1,33 @@
+export const CART_ADD_ITEM = 'cart/addItem'
+export const CART_REMOVE_ITEM = 'cart/removeItem'
+export const CART_INCREASE_ITEM_QUANTITY = 'cart/increaseItemQuantity'
+export const CART_DECREASE_ITEM_QUANTITY = 'cart/decreaseItemQuantity'
+
+export default function cartReducer(state = [], action) {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      return [...state, action.payload]
+    case CART_REMOVE_ITEM:
+      return state.filter(
+        (cartItem) => cartItem.productId !== action.payload.productId
+      )
+
+    case CART_INCREASE_ITEM_QUANTITY:
+      return state.map((cartItem) =>
+        cartItem.productId === action.payload.productId
+          ? { ...cartItem, quantity: cartItem.quantity + 1 }
+          : cartItem
+      )
+    case CART_DECREASE_ITEM_QUANTITY:
+      return state
+        .map((cartItem) =>
+          cartItem.productId === action.payload.productId
+            ? { ...cartItem, quantity: cartItem.quantity - 1 }
+            : cartItem
+        )
+        .filter((cartItem) => cartItem.quantity > 0)
+
+    default:
+      return state
+  }
+}
--- redux_complex_state_using/src/store/store.js
+++ redux_complex_state_using/src/store/store.js
@@ -1,66 +1,83 @@
-import { productList } from './productList'
-import { createStore } from 'redux'
+// import { productList } from './productList'
+import { combineReducers, createStore } from 'redux'
+import cartReducer, {
+  CART_ADD_ITEM,
+  CART_DECREASE_ITEM_QUANTITY,
+  CART_INCREASE_ITEM_QUANTITY,
+  CART_REMOVE_ITEM,
+} from './cartReducer'
+import wishListReducer, {
+  WISHLIST_ADD_ITEM,
+  WISHLIST_REMOVE_ITEM,
+} from './wishListReducer'
+import productReducer from './productReducer'
 
-const initialState = {
-  products: productList,
-  cartItems: [],
-  wishList: [],
-}
+// const initialState = {
+//   products: productList,
+//   cartItems: [],
+//   wishList: [],
+// }
 
-const CART_ADD_ITEM = 'cart/addItem'
-const CART_REMOVE_ITEM = 'cart/removeItem'
-const CART_INCREASE_ITEM_QUANTITY = 'cart/increaseItemQuantity'
-const CART_DECREASE_ITEM_QUANTITY = 'cart/decreaseItemQuantity'
-const WISHLIST_ADD_ITEM = 'wishList/addItem'
-const WISHLIST_REMOVE_ITEM = 'wishList/removeItem'
+// const CART_ADD_ITEM = 'cart/addItem'
+// const CART_REMOVE_ITEM = 'cart/removeItem'
+// const CART_INCREASE_ITEM_QUANTITY = 'cart/increaseItemQuantity'
+// const CART_DECREASE_ITEM_QUANTITY = 'cart/decreaseItemQuantity'
+// const WISHLIST_ADD_ITEM = 'wishList/addItem'
+// const WISHLIST_REMOVE_ITEM = 'wishList/removeItem'
 
-function reducer(state = initialState, action) {
-  console.log(action)
-  switch (action.type) {
-    case CART_ADD_ITEM:
-      return { ...state, cartItems: [...state.cartItems, action.payload] }
-    case CART_REMOVE_ITEM:
-      return {
-        ...state,
-        cartItems: state.cartItems.filter(
-          (cartItem) => cartItem.productId !== action.payload.productId
-        ),
-      }
-    case CART_INCREASE_ITEM_QUANTITY:
-      return {
-        ...state,
-        cartItems: state.cartItems.map((cartItem) =>
-          cartItem.productId === action.payload.productId
-            ? { ...cartItem, quantity: cartItem.quantity + 1 }
-            : cartItem
-        ),
-      }
-    case CART_DECREASE_ITEM_QUANTITY:
-      return {
-        ...state,
-        cartItems: state.cartItems
-          .map((cartItem) =>
-            cartItem.productId === action.payload.productId
-              ? { ...cartItem, quantity: cartItem.quantity - 1 }
-              : cartItem
-          )
-          .filter((cartItem) => cartItem.quantity > 0),
-      }
+// function reducer(state = initialState, action) {
+//   console.log(action)
+//   switch (action.type) {
+//     case CART_ADD_ITEM:
+//       return { ...state, cartItems: [...state.cartItems, action.payload] }
+//     case CART_REMOVE_ITEM:
+//       return {
+//         ...state,
+//         cartItems: state.cartItems.filter(
+//           (cartItem) => cartItem.productId !== action.payload.productId
+//         ),
+//       }
+//     case CART_INCREASE_ITEM_QUANTITY:
+//       return {
+//         ...state,
+//         cartItems: state.cartItems.map((cartItem) =>
+//           cartItem.productId === action.payload.productId
+//             ? { ...cartItem, quantity: cartItem.quantity + 1 }
+//             : cartItem
+//         ),
+//       }
+//     case CART_DECREASE_ITEM_QUANTITY:
+//       return {
+//         ...state,
+//         cartItems: state.cartItems
+//           .map((cartItem) =>
+//             cartItem.productId === action.payload.productId
+//               ? { ...cartItem, quantity: cartItem.quantity - 1 }
+//               : cartItem
+//           )
+//           .filter((cartItem) => cartItem.quantity > 0),
+//       }
 
-    case WISHLIST_ADD_ITEM:
-      return { ...state, wishList: [...state.wishList, action.payload] }
-    case WISHLIST_REMOVE_ITEM:
-      return {
-        ...state,
-        wishList: state.wishList.filter(
-          (wishListItem) => wishListItem.productId !== action.payload.productId
-        ),
-      }
-    default:
-      return state
-  }
-  //   return state
-}
+//     case WISHLIST_ADD_ITEM:
+//       return { ...state, wishList: [...state.wishList, action.payload] }
+//     case WISHLIST_REMOVE_ITEM:
+//       return {
+//         ...state,
+//         wishList: state.wishList.filter(
+//           (wishListItem) => wishListItem.productId !== action.payload.productId
+//         ),
+//       }
+//     default:
+//       return state
+//   }
+//   //   return state
+// }
+
+const reducer = combineReducers({
+  products: productReducer,
+  cartItems: cartReducer,
+  wishList: wishListReducer,
+})
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.())
 console.log(store)
@@ -125,4 +142,5 @@ store.dispatch({
   type: WISHLIST_REMOVE_ITEM,
   payload: { productId: 3 },
 })
+console.log(store.getState())
 export default store
